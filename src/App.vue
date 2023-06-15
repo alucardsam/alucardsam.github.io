0556512be200e13f899739510b08595ba2d0deae
Samuel Adrian Gonzalez Gonzalez <samueladriang@gmail.com>
2023-06-14 18:54:35 -0600

    Se concluye version 1.0.1 para su presentación

--- src/App.vue
+++ src/App.vue
@@ -184,7 +184,7 @@ onMounted(() => {
         <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
           <h3>Desarrollador Web</h3>
           <p class="fst-italic">
-            Actualmente trabajo como un Desarrollador Web para la Universidad de Ciencias y Artes de Chiapas.
+            Actualmente trabajo como un Desarrollador Web para la Universidad de Ciencias y Artes de Chiapas (UNICACH).
           </p>
           <div class="row">
             <div class="col-lg-6">
@@ -212,7 +212,16 @@ onMounted(() => {
             </div>
           </div>
           <p>
-            He realizado diferentes funciones, así como proyectos con diferentes tecnologías.
+            Funciones principales:
+          <ul>
+            <li><i class='bx bx-chevron-right'></i> Diseñar, planeficar y desarrollar sistemas para el procesamiento de la
+              información.</li>
+            <li><i class='bx bx-chevron-right'></i> Administrar y gestionar la captura y el almacenamiento de la
+              información en sistemas de bases de datos relacionales.</li>
+            <li><i class='bx bx-chevron-right'></i> Desarrollar mantenimientos y/o actualizaciones en sistemas web.</li>
+            <li><i class='bx bx-chevron-right'></i> Mantenimiento de equipos de computo, así como supervisar el
+              funcionamiento de una infraestructura.</li>
+          </ul>
           </p>
         </div>
       </div>
@@ -222,7 +231,8 @@ onMounted(() => {
     <div class="container">
       <div class="section-title">
         <h2>Datos</h2>
-        <p>Estas son las estadisticas de todos mis proyectos</p>
+        <p>Mis estadisticas estan divididas en 3 grupos; proyectos freelance, proyectos como encargado directo y proyectos
+          en colaboración</p>
       </div>
       <div class="row no-gutters">
         <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up">
@@ -256,7 +266,7 @@ onMounted(() => {
     <div class="container">
       <div class="section-title">
         <h2>Habilidades</h2>
-        <p>A continuación estas son todas las tecnologías que he utilizado para todos mis proyectos</p>
+        <p>A continuación estas son algunas de las tecnologías que he implementado en la mayoria de mis proyectos:</p>
       </div>
       <div class="row skills-content">
         <div class="mx-auto grid max-w-6xl grid-cols-4 gap-4 sm:grid-cols-8">
@@ -401,7 +411,7 @@ onMounted(() => {
           <h3 class="resume-title">Educación</h3>
           <div class="resume-item">
             <h4>Ingeniero en Sistemas Computacionales</h4>
-            <h5>2006 -2010</h5>
+            <h5>2006 - 2010</h5>
             <p><em>Instituto Tecnológico de Tapachula</em></p>
             <p>Egresado en el año 2010 con título y cédula profesional (Número de cédula 6999208)</p>
           </div>
@@ -409,8 +419,8 @@ onMounted(() => {
         <div class="col-lg-6" data-aos="fade-up-left" data-aos-delay="100">
           <h3 class="resume-title">Professional Experience</h3>
           <div class="resume-item">
-            <h4>Web Developer - UNICACH</h4>
-            <h5>Nov 2011 - Present</h5>
+            <h4>Desarrollador Web - UNICACH</h4>
+            <h5>Nov 2011 - Actualmente</h5>
             <p><em>Tuxtla Gutiérrez, Chiapas, México</em></p>
             <ul>
               <li><i class='bx bx-chevron-right'></i> Administración de Base de Datos (Oracle, MySQL)</li>
@@ -419,7 +429,7 @@ onMounted(() => {
             </ul>
           </div>
           <div class="resume-item">
-            <h4>Web Developer (Remote) - HIGHTECH</h4>
+            <h4>Desarrollador Web (Remote) - HIGHTECH</h4>
             <h5>2017 - 2018</h5>
             <p><em>Tuxtla Gutiérrez, Chiapas, México</em></p>
             <ul>
@@ -436,14 +446,14 @@ onMounted(() => {
     <div class="container">
       <div class="section-title">
         <h2>Portafolio</h2>
-        <p>Dentro de mis 12 años de experiencia he desarrollado como freelance una página web y una app; como
+        <p>Dentro de mis 12 años de experiencia he desarrollado como freelance una página web y como
           desarrollador administrativo en la UNICACH he colaborado y desarrollado diferentes sistemas web, a continuación
-          una breve descripción de mis proyectos</p>
+          una breve descripción de mis proyectos:</p>
       </div>
       <div class="row" data-aos="fade-up">
         <div class="col-lg-12 d-flex justify-content-center">
           <ul id="portfolio-flters">
-            <li data-filter="*" class="filter-active">All</li>
+            <li data-filter="*" class="filter-active">Todos</li>
             <li data-filter=".filter-freelance">Freelance</li>
             <li data-filter=".filter-colaboracion">Colaboración</li>
             <li data-filter=".filter-responsable">Responsable</li>
@@ -568,6 +578,180 @@ onMounted(() => {
             </div>
           </div>
         </div>
+        <div class="col-lg-4 col-md-6 portfolio-item filter-responsable">
+          <div class="portfolio-wrap">
+            <div class="bg-white flex flex-col max-w-sm mx-auto rounded-lg shadow-xl">
+              <img src="@/assets/img/portfolio/responsable02.png" alt="Usuario" class="w-full object-cover" />
+              <div class="portfolio-links">
+                <a href="https://siia.unicach.mx/tramites/" target="_blank"><i class="bx bx-link"></i></a>
+              </div>
+              <div class="bg-gray-900 mx-4 rounded-lg flex items-center gap-2 -mt-8 z-10 shadow-xl py-2 px-6">
+                <h3 class="text-white text-lg font-semibold">TRAMITES EN LÍNEA (CONSTANCIAS)</h3>
+              </div>
+              <div class="px-6 py-4 flex flex-col gap-2">
+                <p class="text-gray-500">
+                  Portal para generar constancias en línea, este sistema cuenta con 2 módulos.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 portfolio-item filter-responsable">
+          <div class="portfolio-wrap">
+            <div class="bg-white flex flex-col max-w-sm mx-auto rounded-lg shadow-xl">
+              <img src="@/assets/img/portfolio/responsable03.png" alt="Usuario" class="object-cover" />
+              <div class="bg-gray-900 mx-4 rounded-lg flex items-center gap-2 -mt-8 z-10 shadow-xl py-2 px-6">
+                <h3 class="text-white text-lg font-semibold">PAGOS ELECTRÓNICOS (SERVICIO Y BITÁCORAS)</h3>
+              </div>
+              <div class="px-6 py-4 flex flex-col gap-2">
+                <p class="text-gray-500">
+                  Sistema para generar links de pagos y obtener respuesta vía POST por cobros de los diferentes conceptos en los diversos sistemas de la Universidad (reinscripción, inscripciones, constancias).
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 portfolio-item filter-responsable">
+          <div class="portfolio-wrap">
+            <div class="bg-white flex flex-col max-w-sm mx-auto rounded-lg shadow-xl">
+              <img src="@/assets/img/portfolio/responsable04.png" alt="Usuario" class="w-full object-cover" />
+              <div class="portfolio-links">
+                <a href="https://siia.unicach.mx/scuid/" target="_blank"><i class="bx bx-link"></i></a>
+              </div>
+              <div class="bg-gray-900 mx-4 rounded-lg flex items-center gap-2 -mt-8 z-10 shadow-xl py-2 px-6">
+                <h3 class="text-white text-lg font-semibold">SICUID</h3>
+              </div>
+              <div class="px-6 py-4 flex flex-col gap-2">
+                <p class="text-gray-500">
+                  Sistema del Centro Universitario de Información y Documentación (CUID) para generar préstamos, renovaciones y pagos de multas, así como alta de libros con ficha y formato MARC 21.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 portfolio-item filter-responsable">
+          <div class="portfolio-wrap">
+            <div class="bg-white flex flex-col max-w-sm mx-auto rounded-lg shadow-xl">
+              <img src="@/assets/img/portfolio/responsable05.png" alt="Usuario" class="w-full object-cover" />
+              <div class="portfolio-links">
+                <a href="https://siia.unicach.mx/scuidusuarios/" target="_blank"><i class="bx bx-link"></i></a>
+              </div>
+              <div class="bg-gray-900 mx-4 rounded-lg flex items-center gap-2 -mt-8 z-10 shadow-xl py-2 px-6">
+                <h3 class="text-white text-lg font-semibold">SICUID USUARIOS</h3>
+              </div>
+              <div class="px-6 py-4 flex flex-col gap-2">
+                <p class="text-gray-500">
+                  Sistema para generar auto préstamos y renovaciones en línea de los libros disponibles de las diversas bibliotecas posicionadas en las diferentes sedes de la Universidad.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 portfolio-item filter-responsable">
+          <div class="portfolio-wrap">
+            <div class="bg-white flex flex-col max-w-sm mx-auto rounded-lg shadow-xl">
+              <img src="@/assets/img/portfolio/responsable06.png" alt="Usuario" class="w-full object-cover" />
+              <div class="portfolio-links">
+                <a href="https://siia.unicach.mx/ssocial/" target="_blank"><i class="bx bx-link"></i></a>
+              </div>
+              <div class="bg-gray-900 mx-4 rounded-lg flex items-center gap-2 -mt-8 z-10 shadow-xl py-2 px-6">
+                <h3 class="text-white text-lg font-semibold">SSOCIAL</h3>
+              </div>
+              <div class="px-6 py-4 flex flex-col gap-2">
+                <p class="text-gray-500">
+                  Sistema para el control del servicio social de los alumnos, este sistema cuenta con 2 módulos.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 portfolio-item filter-responsable">
+          <div class="portfolio-wrap">
+            <div class="bg-white flex flex-col max-w-sm mx-auto rounded-lg shadow-xl">
+              <img src="@/assets/img/portfolio/responsable07.png" alt="Usuario" class="w-full object-cover" />
+              <div class="portfolio-links">
+                <a href="https://siia.unicach.mx/ssocial_alumnos/" target="_blank"><i class="bx bx-link"></i></a>
+              </div>
+              <div class="bg-gray-900 mx-4 rounded-lg flex items-center gap-2 -mt-8 z-10 shadow-xl py-2 px-6">
+                <h3 class="text-white text-lg font-semibold">SSOCIAL ALUMNOS</h3>
+              </div>
+              <div class="px-6 py-4 flex flex-col gap-2">
+                <p class="text-gray-500">
+                  Sistema para el control del servicio social del alumno, vinculación de un programa del servicio social vigente y con disponibilidad, generación de documentos como ficha de la solicitud, carta de presentación y carta compromiso, seguimiento de informes bimestrales y comunicación por medio de notificaciones al correo institucional de las observaciones del coordinador.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 portfolio-item filter-responsable">
+          <div class="portfolio-wrap">
+            <div class="bg-white flex flex-col max-w-sm mx-auto rounded-lg shadow-xl">
+              <img src="@/assets/img/portfolio/responsable08.png" alt="Usuario" class="w-full object-cover" />
+              <div class="portfolio-links">
+                <a href="https://siia.unicach.mx/validacion/" target="_blank"><i class="bx bx-link"></i></a>
+              </div>
+              <div class="bg-gray-900 mx-4 rounded-lg flex items-center gap-2 -mt-8 z-10 shadow-xl py-2 px-6">
+                <h3 class="text-white text-lg font-semibold">VISOR DE USUARIOS</h3>
+              </div>
+              <div class="px-6 py-4 flex flex-col gap-2">
+                <p class="text-gray-500">
+                  Sistema para consultar el estatus e información relevante de todos los usuarios de la Universidad.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 portfolio-item filter-responsable">
+          <div class="portfolio-wrap">
+            <div class="bg-white flex flex-col max-w-sm mx-auto rounded-lg shadow-xl">
+              <img src="@/assets/img/portfolio/responsable09.png" alt="Usuario" class="w-full object-cover" />
+              <div class="portfolio-links">
+                <a href="https://siia.unicach.mx/validacion/" target="_blank"><i class="bx bx-link"></i></a>
+              </div>
+              <div class="bg-gray-900 mx-4 rounded-lg flex items-center gap-2 -mt-8 z-10 shadow-xl py-2 px-6">
+                <h3 class="text-white text-lg font-semibold">TÍTULO ELECTRÓNICO</h3>
+              </div>
+              <div class="px-6 py-4 flex flex-col gap-2">
+                <p class="text-gray-500">
+                  Sistema para generar el envío a la DGP de los últimos títulos registrados, creación del archivo XML sellado con las firmas digitales del rector, secretario general y director.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 portfolio-item filter-responsable">
+          <div class="portfolio-wrap">
+            <div class="bg-white flex flex-col max-w-sm mx-auto rounded-lg shadow-xl">
+              <img src="@/assets/img/portfolio/responsable10.png" alt="Usuario" class="object-cover" />
+              <div class="bg-gray-900 mx-4 rounded-lg flex items-center gap-2 -mt-8 z-10 shadow-xl py-2 px-6">
+                <h3 class="text-white text-lg font-semibold">CREACIÓN DE ARCHIVOS PEM</h3>
+              </div>
+              <div class="px-6 py-4 flex flex-col gap-2">
+                <p class="text-gray-500">
+                  Sistema encargado de generar los archivos PEM para generar el sello de las firmas digitales de los títulos electrónicos a partir de los certificados del SAT.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 portfolio-item filter-responsable">
+          <div class="portfolio-wrap">
+            <div class="bg-white flex flex-col max-w-sm mx-auto rounded-lg shadow-xl">
+              <img src="@/assets/img/portfolio/responsable11.png" alt="Usuario" class="w-full object-cover" />
+              <div class="portfolio-links">
+                <a href="https://siia.unicach.mx/encuesta/" target="_blank"><i class="bx bx-link"></i></a>
+              </div>
+              <div class="bg-gray-900 mx-4 rounded-lg flex items-center gap-2 -mt-8 z-10 shadow-xl py-2 px-6">
+                <h3 class="text-white text-lg font-semibold">ENCUESTA DE SATISFACCIÓN</h3>
+              </div>
+              <div class="px-6 py-4 flex flex-col gap-2">
+                <p class="text-gray-500">
+                  Sistema para generar y configurar las encuestas de satisfacción para alumnos inscritos en el quinto semestre. El alumno debe realizar esta encuesta para tener derecho a inscribirse al siguiente semestre.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -598,6 +782,9 @@ onMounted(() => {
                   href="https://api.whatsapp.com/send?phone=529611135699&amp;text=Hola%2C+revise+tu+p%C3%A1gina+web+y+quiero+contactarme+contigo">+52
                   961 113 5699</a></p>
             </div>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d61129.858479175615!2d-93.1707694296455!3d16.745993463026686!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85ecd85611d06e8f%3A0xdd78bd7b4662216a!2sTuxtla%20Guti%C3%A9rrez%2C%20Chis.!5e0!3m2!1ses-419!2smx!4v1686789148318!5m2!1ses-419!2smx"
+              frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
           </div>
         </div>
         <!-- <div class="col-lg-12 mt-5 mt-lg-0 d-flex align-items-stretch">
